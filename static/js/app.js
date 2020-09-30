d8d7bfb4875b11af500833040cc574208df555d7
bejirose <65885719+bejirose@users.noreply.github.com>
2020-09-30 00:42:49 -0700

    Plotly Challenge

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,3 @@
-// Plan
-
-// init function 
-function init() {
-  // 1) Fill out dropdown with all of the ids
-  
-}
-
-// 2) Calls a buildPage function that draws the chart and the panel for the first one
-
-// buildPage function 
-// 1) That takes one parameter, which is the subject ID
-// 2) Draws our plotly charts and fills the panel
-
-// Need an event listener for the dropdown
-// optionChanged function
-// - That takes as a parameter the user selection
-
 var dataSet = {};
 
 function dispIDemography(id){
@@ -52,16 +34,6 @@ function buildCharts(id) {
 
   console.log(chartData);
   
-  // sort the dataset to get the top10 OTUs for the selected ID : choose the fist array
-  // chartData = chartData[0].sort(function(a,b) {
-  //     return (a-b);
-  // });
-
-  // console.log(chartData);
-  // var sampleValues = sortedData.slice(0,10); // .sort(function(a,b) {//   return (a-b);// });
-  // var otuLabels = chartData[0].otu_labels.slice(0,10); // // get otu_labels for the top 10 OTU's to hovertext for the chart.
-  // var otuIds = chartData[0].otu_ids.slice(0,10).map(x => 'otu '+x); //get otu_ids for the top 10 OTU's to labels for the bar chart.
-
   var sampleValues = chartData[0].sample_values.slice(0,10); // .sort(function(a,b) {//   return (a-b);// });
   var otuLabels = chartData[0].otu_labels.slice(0,10); // // get otu_labels for the top 10 OTU's to hovertext for the chart.
   var otuIds = chartData[0].otu_ids.slice(0,10).map(x => 'otu '+x); //get otu_ids for the top 10 OTU's to labels for the bar chart.
@@ -81,7 +53,7 @@ function buildCharts(id) {
   
   // Apply the group bar mode to the layout
   var layout = {
-    title: "OTU Chart",
+    title: "<b>OTU Chart</b>",
     margin: {
       l: 100,
       r: 100,
@@ -92,18 +64,20 @@ function buildCharts(id) {
 
   // Render the plot to the div tag with id "plot"
   Plotly.newPlot("bar", data, layout);
-  desired_maximum_marker_size = 40;
+  
   // Trace Data to draw bubble chart
+  maxiMarkerSize = 40;
   size = [];
   console.log(chartData[0].sample_values.slice(0,20));
-  //for (i = 0; i < chartData[0].sample_values.length; i++) {
+ 
   // Add the size of the bubles for the top  20 items
+  //for (i = 0; i < chartData[0].sample_values.length; i++) {
   for (i = 0; i < 20; i++) {
      if ( chartData[0].sample_values[i] > 20) {
       size[i] = 1+ chartData[0].sample_values[i] ;
     }
     else {
-      size[i] = 1+ 20 ;
+      size[i] = 1+ 20 ; // Adding this to draw sample_value < 20 a bit bigger to make it visible
     }
   }
 
@@ -115,68 +89,59 @@ function buildCharts(id) {
     mode: 'markers',
     marker: {
       size: size,
-      //set 'sizeref' to an 'ideal' size given by the formula sizeref = 2. * max(array_of_size_values) / (desired_maximum_marker_size ** 2)
-      sizeref: (2.0 * Math.max(...size) / (desired_maximum_marker_size**2)),
-      sizemode: 'area'
+      sizeref: (2.0 * Math.max(...size) / (maxiMarkerSize**2)),
+      sizemode: 'area',
+      color: chartData[0].sample_values.slice(0,20)
     }
 
   }];
 
-
-  // Apply the group bar mode to the layout
   var layout = {
-    title: "OTU bubble Chart"
+    title: "<b>OTU Bubble Chart</b>"
   };
 
-    // Render the plot to the div tag with id "plot"
   Plotly.newPlot("bubble", data, layout);
-
+ 
   // Gauge plot - Bonus
   console.log(`Weekly Frequency for ${id} = ${wFrequency}`);
   
-  data = [{
-      domain: { x: [0, 9], y: [0, 9] },
-      value: wFrequency,
-      title: { text: "Speed" },
-      type: "indicator",
-      mode: "gauge+number",
-      //values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
-      //values: [wFrequency/9,wFrequency/9, wFrequency/9,wFrequency/9, wFrequency/9,wFrequency/9, wFrequency/9,wFrequency/9, wFrequency/9,wFrequency/9],
-        //rotation: 90,
-        // text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
-        // textinfo: 'text',
-        // textposition:'inside',
-        // marker: {colors:['rgba(0, 105, 11, .5)', 'rgba(10, 120, 22, .5)',
-        //                 'rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
-        //                  'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
-        //                  'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
-        //                  'rgba(240, 230, 215, .5)', 'rgba(255, 255, 255, 0)']},
-        //labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1',''],
-        //hoverinfo: 'label',
-        // hole: .5,
-        // type: 'pie',
-        // showlegend: false
-  
+  var data = [{
+    type: "indicator",
+    mode: "gauge+number",
+    value: wFrequency,
+    gauge: {
+      axis: { range: [null, 10], tickwidth: 1, tickcolor: "darkblue" },
+      bar: { color: "purple" },
+      bgcolor: "white",
+      borderwidth: 2,
+      bordercolor: "gray",
+      steps: [
+        { range: [9,10], color: 'rgba(0, 105, 11, .5)' },
+        { range: [8,9], color: 'rgba(10, 120, 22, .5)' },
+        { range: [7,8], color: 'rgba(14, 127, 0, .5)'},
+        { range: [6,7], color: 'rgba(110, 154, 22, .5)'},
+        { range: [5,6], color: 'rgba(170, 202, 42, .5)'},
+        { range: [4,5], color: 'rgba(202, 209, 95, .5)'},
+        { range: [3,4], color: 'rgba(210, 206, 145, .5)'},
+        { range: [2,3], color: 'rgba(232, 226, 202, .5)'},
+        { range: [1,2], color: 'rgba(240, 230, 215, .5)'},
+        { range: [0, 1], color: 'rgba(255, 255, 255, 0)'}
+      ],
+      threshold: {
+        line: { color: "red", width: 4 },
+        thickness: 0.75,
+        value: 490
+      }
+    }
   }];
   
-  layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+  var layout = {
+    title: "<b>Washing Frequency</b><br>[Scrubs per Week]",
+    margin: { r: 25, l: 25, b: 25 },
+    font: { color: "black", family: "Arial" }
+  };
+  
   Plotly.newPlot('gauge', data, layout);
-  // sortedData.sort(function(a,b) {
-  //    return (a-b);
-  //  });
-  //   // Fill up the Panel
-  
-  
-    // Filter data.samples based on subject
-    // The array that you get back you are interested in [0]
-
-    // Use dot notation to get at .otu_ids, .otu_labels, .otu_sample_values
-    // Use slice for the horizontal bar chart
-
-    // Plotly charts
-    // Horizonatal bar chart- orientation: "h"
-
-
 }
 
 
